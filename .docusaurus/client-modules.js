a9b52c7dc570cd6aea30f089c5a1d8030c391e4b
matheus-mcosta <matheus.costa@inf.ufrgs.br>
2024-10-24 12:30:25 -0300

    fix: missing .docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/opt/docusaurus/node_modules/infima/dist/css/default/default.css"),
+  require("/opt/docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/opt/docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/opt/docusaurus/src/css/custom.css"),
+  require("/opt/docusaurus/node_modules/docusaurus-theme-redoc/dist/custom.css"),
+  require("/opt/docusaurus/node_modules/docusaurus-theme-redoc/dist/global.js"),
+];
